076be76b2dd16ac36603ecd35b60a0a01174175e
bcondict <jesus.junc10@gmail.com>
2025-05-14 22:44:22 -0500

    upload (assets / animations): upload the movement of bots, and the character, with the asset of the castle

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using System.Collections;
+
+public class EnemyAI : MonoBehaviour
+{
+    private State state;
+    private EnemyPathfinding enemyPathfinding;
+
+    private enum State
+    {
+        Roaming
+    }
+
+    private void Awake()
+    {
+        enemyPathfinding = GetComponent<EnemyPathfinding>();
+        state = State.Roaming;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(RoamingRoutine());
+    }
+
+    private IEnumerator RoamingRoutine()
+    {
+        while (state == State.Roaming)
+        {
+            Vector2 roamingPosition = GetRoamingPosition();
+            enemyPathfinding.MoveTo(roamingPosition);
+            yield return new WaitForSeconds(2f);
+        }
+    }
+
+    private Vector2 GetRoamingPosition()
+    {
+        return new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
+}
